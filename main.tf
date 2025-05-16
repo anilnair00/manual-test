38a20acbe8b02e8192888248f09f7395615f1d85
anilnair00 <168839295+anilnair00@users.noreply.github.com>
2025-05-16 09:19:15 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,20 @@ resource "harness_platform_project" "project" {
     identifier = "terraform_test_project"  
     org_id    = "default"  
 }
+resource "harness_platform_connector_azure_cloud_provider" "inherit_from_delegate_user_assigned_managed_identity" {
+  identifier  = "identifier"
+  name        = "name"
+  description = "example"
+  tags        = ["foo:bar"]
+
+  credentials {
+    type = "InheritFromDelegate"
+    azure_inherit_from_delegate_details {
+      auth {
+        azure_msi_auth_ua {
+          client_id = "client_id"
+        }
+        type = "UserAssignedManagedIdentity"
+      }
+    }
+  }
